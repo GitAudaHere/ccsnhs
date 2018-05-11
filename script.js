b2fa129021431f46bb1d7c6bda30289a17993aec
Cheney <cheney.ni@ccs.us>
2018-05-11 10:10:03 -0400

    Menu animation more modular. Moved up menu

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Dropdown menu sliding
 $(document).ready(function(){
+    var slideSpeed = 250;
     $(".dropdown-menu").hover(function(){
-        $(this).siblings().find("div").slideUp(300);
-        $(this).find("div").slideDown(300);
+        $(this).siblings().find("div").slideUp(slideSpeed);
+        $(this).find("div").slideDown(slideSpeed);
     }, function() {
-        $(this).find("div").slideUp(300);
+        $(this).find("div").slideUp(slideSpeed);
     });
 });
