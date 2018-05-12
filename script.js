c96cc5e5ae5f9bedee839edb57d55e95b67ef158
Cheney <cheney.ni@ccs.us>
2018-05-12 00:46:59 -0400

    Color change animation for menu on hover.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-// Dropdown menu sliding
 $(document).ready(function(){
-    var speed = 250;
+    var animationSpeed = 250;
+    /* Menu dropdown animation */
     $(".dropdown-menu").hover(function(){
-        $(this).siblings().find("div").slideUp(speed);
-        $(this).find("div").slideDown(speed);
-
-    }, function() {
-        $(this).find("div").slideUp(speed);
+        $(this).siblings().find("div").slideUp(animationSpeed);
+        $(this).find("div").slideDown(animationSpeed);
+    }, function(){
+        $(this).find("div").slideUp(animationSpeed);
+    });
+    /* Menu item highlight animation */
+    $(".dropdown").hover(function(){
+        $(this).animate({color: "#42c5f4"});
+        $(this).css({paddingBottom: "30px"});
+        $(this).css({borderBottomWidth: "2px"});
+    }, function(){
+        $(this).animate({color: "#FFFFFF"}, animationSpeed);
+        $(this).css({paddingBottom: "10px"});
+        $(this).css({borderBottomWidth: "0px"});
     });
 });
