f5f0f6ce75d52cedebf9fdcd47c9bbb6b58f83ee
Marshall <marshall@lentzfam.com>
2018-05-21 09:19:25 -0400

    Animated the back to top button.

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 $(document).ready(function(){
-    var animationSpeed = 150;
+    var duration = 150;
 
     /* Menu dropdown animation */
     $(".dropdown-menu").hover(function(){
-        $(this).siblings().find("div").fadeOut(animationSpeed);
-        $(this).find("div").stop(true, true).fadeIn(animationSpeed);
+        $(this).siblings().find("div").fadeOut(duration);
+        $(this).find("div").stop(true, true).fadeIn(duration);
     }, function(){
-        $(this).find("div").fadeOut(animationSpeed);
-        $(".dropdown").stop(true, true).animate({color: "#FFFFFF"}, animationSpeed);
-        $(".dropdown").stop(true, true).css({paddingBottom: "10px"}, animationSpeed);
-        $(".dropdown").stop(true, true).css({borderBottomWidth: "0px"}, animationSpeed);
+        $(this).find("div").fadeOut(duration);
+        $(".dropdown").stop(true, true).animate({color: "#FFFFFF"}, duration);
+        $(".dropdown").stop(true, true).css({paddingBottom: "10px"}, duration);
+        $(".dropdown").stop(true, true).css({borderBottomWidth: "0px"}, duration);
     });
 
     /* Menu item highlight animation */
     $(".dropdown").hover(function(){
-        $(this).animate({color: "#42c5f4"}, animationSpeed);
-        $(this).css({paddingBottom: "30px"}, animationSpeed);
-        $(this).css({borderBottomWidth: "2px"}, animationSpeed);
+        $(this).animate({color: "#42c5f4"}, duration);
+        $(this).css({paddingBottom: "30px"}, duration);
+        $(this).css({borderBottomWidth: "2px"}, duration);
     }, function(){
         if($(".dropdown-content:hover").length == 0)
         {
-            $(this).stop(true, false).animate({color: "#FFFFFF"}, animationSpeed);
-            $(this).stop(true, false).css({paddingBottom: "10px"}, animationSpeed);
-            $(this).stop(true, false).css({borderBottomWidth: "0px"}, animationSpeed);
+            $(this).stop(true, false).animate({color: "#FFFFFF"}, duration);
+            $(this).stop(true, false).css({paddingBottom: "10px"}, duration);
+            $(this).stop(true, false).css({borderBottomWidth: "0px"}, duration);
         }
     });
 
     var bylaws_open = false;
     $("#bylaws-dropdown-btn").click(function(){
         if (bylaws_open == false){
-            $("#bylaws").stop(true, false).slideDown(animationSpeed * 10);
+            $("#bylaws").stop(true, false).slideDown(duration * 10);
             bylaws_open = true;
         }
         else{
-            $("#bylaws").stop(true, false).slideUp(animationSpeed * 10);
+            $("#bylaws").stop(true, false).slideUp(duration * 10);
             bylaws_open = false;
         }
     });
+
+    window.onscroll = function() {scroll()};
+
+    function scroll() {
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $("#top-button").fadeIn(duration);
+        } else {
+            $("#top-button").fadeOut(duration / 1.5);
+        }
+    }
+
+    function backToTop() {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    }
 
     $("#top-button").click(function(){
         backToTop();
     });
 });
-
-window.onscroll = function() {scroll()};
-
-function scroll() {
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("top-button").style.display = "block";
-    } else {
-        document.getElementById("top-button").style.display = "none";
-    }
-}
-
-function backToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-}
