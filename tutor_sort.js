8646caca0066c683ce549be9fffa171fa2bc852f
Cheney Ni <cheney.ni@ccs.us>
2018-09-16 00:37:50 -0400

    JS automatically adds tutors to table

---
+++ tutor_sort.js
@@ -0,0 +1,46 @@
+
+/* `subject` is an array of strings */
+
+class TutorRow {
+    constructor(subject, tutor, availability) {
+        this.subject = subject;
+        this.tutor = tutor;
+        this.availability = availability;
+    }
+}
+
+var tutorRows = [
+    new TutorRow("Math - Algebra, Calculus, Precalculus", "Cheney Ni", "1st, 2nd (Mon, Wed, Fri), 3rd (every day)"),
+    new TutorRow("SAT and ACT Prep", "Marshall Lentz", "1st, 2nd (Mon, Wed, Fri), 3rd (every day)"),
+    new TutorRow("Literature - American, World", "Austin James", "3rd (every day), 5th (odd days)"),
+    new TutorRow("Math - Algebra, Precalculus", "Austin James", "3rd (every day), 5th (odd days)")
+];
+
+tutorRows.sort(function(a, b){
+    var keyA = a.subject;
+    var keyB = b.subject;
+    if (keyA < keyB) return -1;
+    if (keyB < keyA) return 1;
+    return 0;
+});
+
+$(document).ready(function(){
+    var tableBody = document.getElementById("tutoring-table").children[2];
+    for (var i = 0; i < tutorRows.length; i++) {
+        var row = document.createElement("tr");
+        var subjectName = document.createElement("td");
+        var tutorName = document.createElement("td");
+        var availableTimes = document.createElement("td");
+        var curTutorRow = tutorRows[i];
+        row.appendChild(subjectName);
+        row.appendChild(tutorName);
+        row.appendChild(availableTimes);
+        subjectName.appendChild(document.createTextNode(curTutorRow.subject));
+        tutorName.appendChild(document.createTextNode(curTutorRow.tutor));
+        availableTimes.appendChild(document.createTextNode(curTutorRow.availability));
+        subjectName.classList.add("keyrow");  // Not supported by IE9
+        tutorName.classList.add("keyrow");  // Not supported by IE9
+        availableTimes.classList.add("keyrow");  // Not supported by IE9
+        tableBody.appendChild(row);
+    }
+});
