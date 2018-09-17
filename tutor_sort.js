b2649737b5da99858f3a1323f3643b75fb1f4108
Cheney Ni <cheney.ni@ccs.us>
2018-09-17 09:43:41 -0400

    Tutor names are links to send email

--- tutor_sort.js
+++ tutor_sort.js
@@ -31,13 +31,24 @@ $(document).ready(function(){
         var subjectName = document.createElement("td");
         var tutorName = document.createElement("td");
         var availableTimes = document.createElement("td");
+        var emailLink = document.createElement("a");
+
         var curTutorRow = tutorRows[i];
+
         row.appendChild(subjectName);
         row.appendChild(tutorName);
         row.appendChild(availableTimes);
+
         subjectName.appendChild(document.createTextNode(curTutorRow.subject));
-        tutorName.appendChild(document.createTextNode(curTutorRow.tutor));
+        tutorName.appendChild(emailLink);
+        emailLink.appendChild(document.createTextNode(curTutorRow.tutor));
+
+        emailAddress = curTutorRow.tutor.split(" ")[0] + "." + curTutorRow.tutor.split(" ")[1] + "@ccs.us";
+        emailLink.href = "mailto:" + emailAddress.toLowerCase();
+        emailLink.target = "_blank";
+
         availableTimes.appendChild(document.createTextNode(curTutorRow.availability));
+
         subjectName.classList.add("keyrow");  // Not supported by IE9
         tutorName.classList.add("keyrow");  // Not supported by IE9
         availableTimes.classList.add("keyrow");  // Not supported by IE9
